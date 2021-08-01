d2382c55864886e2c12d54c0dad6aca270c2949d
Dan Pollock <griz11@twc.com>
2021-08-01 16:56:10 -0500

    Add files via upload

---
+++ panther_bringup/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_a655e.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_a655e.dir/testCXXCompiler.cxx.o"
+  "cmTC_a655e.pdb"
+  "cmTC_a655e"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_a655e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
